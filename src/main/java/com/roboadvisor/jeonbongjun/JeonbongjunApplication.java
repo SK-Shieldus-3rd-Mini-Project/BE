168da2ad9ee67820071111dc3ffe925e062eb37c
o11117 <115409472+o11117@users.noreply.github.com>
2025-10-20 10:56:45 +0900

    initial commit

---
+++ src/main/java/com/roboadvisor/jeonbongjun/JeonbongjunApplication.java
@@ -0,0 +1,13 @@
+package com.roboadvisor.jeonbongjun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeonbongjunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeonbongjunApplication.class, args);
+	}
+
+}
